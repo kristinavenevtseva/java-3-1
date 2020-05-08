36c7e1177b7b3e99d725c1f228558cc26797f440
kristinavenevtseva <63412659+kristinavenevtseva@users.noreply.github.com>
2020-05-08 13:37:06 +0300

    added all

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        float price = 4555;
+        int every = 20;
+        int mile = 1;
+        int bonus = (int) (price/every*mile);
+        System.out.println(bonus);
+    }
+}
